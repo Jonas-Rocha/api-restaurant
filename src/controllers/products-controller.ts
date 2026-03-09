b4a8cd0a6d3a836395c445e45f43301b4e5dfef6
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-03-09 14:29:27 -0300

    Criando o Middleware de tratamento de Exceções

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
+      //   throw new AppError("Erro de teste", 501);  << erro de teste
       return response.json({ message: "Ok" });
     } catch (error) {
       next(error); //NextFunction para poder tratar erros assincronos
