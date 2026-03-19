aa010cb2b459d19be44583b09cc7be81d27def67
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-03-19 13:07:46 -0300

    Listando mesas

---
+++ src/controllers/tables-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex";
+import { table } from "console";
+
+class TablesController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const tables = await knex<TableRepository>("tables")
+        .select()
+        .orderBy("table_number");
+
+      response.json(tables);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { TablesController };
